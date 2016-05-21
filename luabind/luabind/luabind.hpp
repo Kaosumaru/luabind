b8052b1ace5313ffc494855a3b62cd3f43721fb0
Mateusz Borycki <Mateusz Borycki>
2016-05-21 20:21:08 +0200

    CallFunction now supports void return type

--- luabind/luabind/luabind.hpp
+++ luabind/luabind/luabind.hpp
@@ -6,23 +6,46 @@
 
 namespace luabind
 {
+    namespace impl
+    {
+        template<typename ReturnType>
+        struct ReturnInfo
+        {
+            static constexpr int arity = 1;
+            static ReturnType get(LuaStackStream &ss)
+            {
+                ReturnType r;
+                ss >> r;
+                return r;
+            }
+        };
+
+        template<>
+        struct ReturnInfo<void>
+        {
+            static constexpr int arity = 0;
+            static void get(LuaStackStream &ss) { }
+        };
+
+    }
+
     template<typename ReturnType, typename ...Args>
     ReturnType CallFunction(lua_State* lua, const char* function_name, Args&&... args)
     {
         LuaStackStream ss{ lua };
 
-        lua_getglobal(lua, function_name);
+        int result = lua_getglobal(lua, function_name);
 
         auto fill_args = { 0,
             ((ss << args), 0)...
         };
 
+        using ReturnInfo = impl::ReturnInfo<ReturnType>;
         static constexpr int arity = sizeof...(args);
-        lua_call(lua, arity, 1);
 
-        ReturnType r;
-        ss >> r;
-        return r;
+        lua_call(lua, arity, ReturnInfo::arity);
+
+        return ReturnInfo::get(ss);
     }
 
 };
